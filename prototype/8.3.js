b8fab5d7d7cbe7e964eca6f3cabc306d71b35c89
Dmitry Razumny <nepalevnodigital@gmail.com>
2022-07-04 17:18:50 +0300

    вроде сделал myCall\myBind

--- prototype/8.3.js
+++ prototype/8.3.js
@@ -1,24 +1,21 @@
-
-
 function f() {
-    console.log("Hello!");
-  }
-  Function.prototype.defer = function(ms) {
-    setTimeout(this, ms);
-  }
-  
-  f.defer(1000); // выведет "Hello!" через 1 секунду
+  console.log('Hello!');
+}
+Function.prototype.defer = function (ms) {
+  setTimeout(this, ms);
+};
 
-  function j(a, b) {
-    console.log( a + b );
-  }
-  
-  Function.prototype.defer = function (ms) {
-      let fn = this;
-      return function(...rest) {
-          setTimeout(() => fn.apply(this, rest), ms);
-      }
-  }
+f.defer(1000); // выведет "Hello!" через 1 секунду
 
-  f.defer(1000)(1, 2); // выведет 3 через 1 секунду.
-  
+function j(a, b) {
+  console.log(a + b);
+}
+
+Function.prototype.defer = function (ms) {
+  const fn = this;
+  return function (...rest) {
+    setTimeout(() => fn.apply(this, rest), ms);
+  };
+};
+
+f.defer(1000)(1, 2); // выведет 3 через 1 секунду.
--- prototype/bindCallApply.js
+++ prototype/bindCallApply.js
@@ -1,9 +1,15 @@
-const zpInCity = {
-  moscow: 'over9000',
+const moscow = {
+  zp: 'over9000',
+};
+const piter = {
+  zp: 'over4500',
+};
+const regioni = {
+  zp: true,
 };
 
-function zp(job, staj) {
-  console.log(`Зп:${this.moscow}, Работа:${job}, Стаж:${staj}`);
+function zpha(job, staj) {
+  console.log(`Зп:${this.zp}, Работа:${job}, Стаж:${staj}`);
 }
 
 /* function myBind(fn, obj, ...rest) {
@@ -13,15 +19,28 @@ function zp(job, staj) {
     obj.fn(...rest);
   };
 }
-myBind(zp, zpInCity, 'frontend', '9')(); */
+myBind(zpha, zpInCity, 'frontend', '9')(); */
 
+Function.prototype.myApply = function (obj,args) {
+  obj.fn = this;
+  return obj.fn(...args);
+};
 
-Function.prototype.myBind = function (obj,...rest){
-    Function.__proto__=obj;
-    let f = this;
-    console.log(f);
-    ()=>f(rest);
-}
+zpha.myApply(moscow, ['frontend']);
 
-zp.myBind(zpInCity,'frontend', '9')();
+zpha.myApply(piter, ['1']);
+
+zpha.myApply(regioni, [1, 2]);
+
+Function.prototype.myBind = function (obj,...args) {
+  obj.fn = this;
+  return function () {
+    obj.fn(...args);
+  }
+};
+zpha.myBind(moscow, 'frontend', '9')();
+
+zpha.myBind(piter, '1')();
+
+zpha.myBind(regioni, 1, 2)();
 
