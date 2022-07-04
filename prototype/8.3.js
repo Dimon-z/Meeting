9311222bebce455de74802d2b1b9ef7fb52e888d
Dmitry Razumny <nepalevnodigital@gmail.com>
2022-07-04 12:09:06 +0300

    подправил по мелочи

--- prototype/8.3.js
+++ prototype/8.3.js
@@ -1,7 +1,7 @@
 
 
 function f() {
-    alert("Hello!");
+    console.log("Hello!");
   }
   Function.prototype.defer = function(ms) {
     setTimeout(this, ms);
@@ -9,11 +9,16 @@ function f() {
   
   f.defer(1000); // выведет "Hello!" через 1 секунду
 
-  
-  Function.prototype.defer = function(ms)
-
-  function f(a, b) {
-    alert( a + b );
+  function j(a, b) {
+    console.log( a + b );
   }
   
+  Function.prototype.defer = function (ms) {
+      let fn = this;
+      return function(...rest) {
+          setTimeout(() => fn.apply(this, rest), ms);
+      }
+  }
+
   f.defer(1000)(1, 2); // выведет 3 через 1 секунду.
+  
--- prototype/bindCallApply.js
+++ prototype/bindCallApply.js
@@ -6,11 +6,22 @@ function zp(job, staj) {
   console.log(`Зп:${this.moscow}, Работа:${job}, Стаж:${staj}`);
 }
 
-function myBind(fn, obj, ...rest) {
+/* function myBind(fn, obj, ...rest) {
   return function () {
     Object.fn = fn;
     obj.__proto__ = Object;
     obj.fn(...rest);
   };
 }
-myBind(zp, zpInCity, 'frontend', '9')();
+myBind(zp, zpInCity, 'frontend', '9')(); */
+
+
+Function.prototype.myBind = function (obj,...rest){
+    Function.__proto__=obj;
+    let f = this;
+    console.log(f);
+    ()=>f(rest);
+}
+
+zp.myBind(zpInCity,'frontend', '9')();
+
