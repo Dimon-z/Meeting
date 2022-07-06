5885b30e9e2fe0bc77af8b74524cd56883139957
Dmitry Razumny <nepalevnodigital@gmail.com>
2022-07-06 14:44:53 +0300

    поправил 8.3(закралась упячка)

--- prototype/8.3.js
+++ prototype/8.3.js
@@ -7,7 +7,7 @@ Function.prototype.defer = function (ms) {
 
 f.defer(1000); // выведет "Hello!" через 1 секунду
 
-function j(a, b) {
+function sum(a, b) {
   console.log(a + b);
 }
 
@@ -18,4 +18,4 @@ Function.prototype.defer = function (ms) {
   };
 };
 
-f.defer(1000)(1, 2); // выведет 3 через 1 секунду.
+sum.defer(1000)(1, 2); // выведет 3 через 1 секунду.
