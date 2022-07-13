3d2aad52e8458ba523ba15c8b385f1de5754e3ce
Dmitry Razumny <nepalevnodigital@gmail.com>
2022-07-13 23:49:56 +0300

    сделал 9.1 9.2

---
+++ Classes/9.2.js
@@ -0,0 +1,60 @@
+/* class Piton {
+  constructor(name, hp, maxStamina, healAmount, farmSpeed, attackDMG) {
+    this.name = name;
+    this.hp = hp;
+    this.maxStamina = maxStamina;
+    this.farmSpeed = farmSpeed;
+    this.healAmount = healAmount;
+    this.attackDMG = attackDMG;
+    this.stamina = 0;
+  }
+
+  farm() { console.log(`я фармлю ${this.farmSpeed} шт. золотых монет`); }
+
+  move() { console.log(`я иду на ${this.stamina} метров`); }
+
+  selfHeal() { console.log(`я лечусь на ${this.healAmount} хп`); }
+
+  attack(targetName) { console.log(`я наношу  ${targetName} ${this.attackDMG} урона`); }
+
+  regenerateStamina() { this.stamina = this.maxStamina; }
+}
+
+const boevoyPiton = new Piton('kusya', 120, 80, 20, 10, 30);
+
+class MagePiton extends Piton {
+  constructor(name, hp, maxStamina, healAmount, farmSpeed, attackDMG, mageDMG) {
+    super(name, hp, maxStamina, healAmount, farmSpeed, attackDMG);
+    this.mageDMG = mageDMG;
+  }
+
+  mageAttack(targetName) { super.attack(targetName); console.log(`и запускаю в ${targetName} фаерболл на  ${this.mageDMG} урона`); }
+}
+
+const piton = new MagePiton('mag', 80, 100, 25, 0, 1, 12);
+piton.mageAttack('Baks');
+piton.move();
+piton.attack('jopa');
+piton.regenerateStamina();
+piton.move();
+piton.farm();
+ */
+
+class Animal {
+
+    constructor(name) {
+      this.name = name;
+    }
+  
+  }
+  
+  class Rabbit extends Animal {
+    constructor(name) {
+      super(name);
+      this.created = Date.now();
+    }
+  }
+  
+  let rabbit = new Rabbit("Белый кролик"); 
+  console.log(rabbit);
+
--- Classes/задумка.js
+++ Classes/задумка.js
@@ -1,4 +1,4 @@
-/* class Piton {
+class Piton {
   constructor(name, hp, maxStamina, healAmount, farmSpeed, attackDMG) {
     this.name = name;
     this.hp = hp;
@@ -26,41 +26,3 @@ boevoyPiton.move();
 boevoyPiton.selfHeal();
 boevoyPiton.regenerateStamina();
 boevoyPiton.move();
- */
-class Clock {
-  constructor({ template }) {
-    this.template = template;
-  }
-
-  render() {
-    const date = new Date();
-
-    let hours = date.getHours();
-    if (hours < 10) hours = `0${hours}`;
-
-    let minutes = date.getMinutes();
-    if (minutes < 10) minutes = `0${minutes}`;
-
-    let seconds = date.getSeconds();
-    if (seconds < 10) seconds = `0${seconds}`;
-
-    const output = this.template
-      .replace('h', hours)
-      .replace('m', minutes)
-      .replace('s', seconds);
-
-    console.log(output);
-  }
-
-  stop() {
-    clearInterval(this.timer);
-  }
-
-  start() {
-    this.render();
-    this.timer = setInterval(() => this.render(), 10);
-  }
-}
-
-const clock = new Clock({ template: 'h:m:s' });
-clock.start();
