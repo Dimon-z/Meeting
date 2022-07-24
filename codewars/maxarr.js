cb04801044a1b555aaf7ccdb593d0e76e0ce78be
Dmitry Razumny <nepalevnodigital@gmail.com>
2022-07-24 20:23:53 +0300

    нашел задачку

---
+++ codewars/maxarr.js
@@ -0,0 +1,11 @@
+const maxSequence = function (arr) {
+    max = 0;
+  for (let index1 = 0; index1 < array.length; index1++) {
+    const element = array[index1];
+    for (let index2 = 0; index2 < array.length; index2++) {
+        const element = array[index2];
+        let max = arr[index1]+arr[index2];
+    }
+  }
+};
+ берем sum = 0 если sum + a[i] > sum обновляем sum на нет и суда нет. 
