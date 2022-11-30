7cadb3e0cb6ef01d3f20f13114164bc9f34e744c
Dmitry Razumny <nepalevnodigital@gmail.com>
2022-11-30 17:36:33 +0300

    2 more codewars tasks

--- codewars/towers.js
+++ codewars/towers.js
@@ -1,19 +1,18 @@
 function towerBuilder(nFloors) {
-    let floor = 1;
-    let tower = [];
-    let i;
-    tower.push(floor);
-  for ( i = 0; i < nFloors -1; i++) {
-    floor = floor +2;
+  let floor = 1;
+  const tower = [];
+  let i;
+  tower.push(floor);
+  for (i = 0; i < nFloors - 1; i++) {
+    floor += 2;
     tower.push(floor);
   }
   let answer = [];
-  answer = tower.map(element=>'*'.repeat(element));
+  answer = tower.map((element) => '*'.repeat(element));
   for (let index = nFloors; index >= 0; index--) {
-    let spaces = tower[i] - tower[index];
-    answer[index].push(' '.repeat(spaces/2))
+    const spaces = tower[i] - tower[index];
+    answer[index].push(' '.repeat(spaces / 2));
   }
   return answer;
 }
 console.log(towerBuilder(5));
-
