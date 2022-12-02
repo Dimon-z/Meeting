b2cb879a33ef831fa5889fd19ddb6546bf847cee
Dmitry Razumny <nepalevnodigital@gmail.com>
2022-12-02 08:18:23 +0300

    asyncAwait

--- Promises/myPromise.js
+++ Promises/myPromise.js
@@ -1,21 +1,32 @@
 class MyPromise {
 constructor(resolveOrRejectCB){
-    let onResolve;
-    let onReject;
-    this.then = function(cb){
+    this.resolveOrRejectCB = resolveOrRejectCB(resolve);
+};
+    onResolve;
+    fulfilled = false;
+    called = false;
+    value;
+
+    then = function(cb){
         onResolve = cb;
         return this;
     }
-    this.catch = function(cb){
+
+    catch = function(cb){
 
         onReject = cb;
         return this;
     }
-    this.resolve = resolveOrRejectCB;
-}
-resolve(data){
-onResolve(data);
-}
+
+    resolve(val) {
+        fulfilled = true;
+        value = val;
+
+        if (typeof onResolve === 'function'){
+            onResolve(val);
+            called = true;
+        }
+    }
 }
 
-new MyPromise((resolve) => setTimeout(() => resolve(1000), 1000)).then(val => console.log(val));
+new MyPromise((resolve) => setTimeout(() => resolve(1), 1000)).then(val => console.log(val));
