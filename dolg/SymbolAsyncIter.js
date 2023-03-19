8329dc990b1dc4ef9890ce8beacfecf6ed8a446d
Dmitry Razumny <nepalevnodigital@gmail.com>
2023-03-19 08:18:40 +0300

    symbolasync`

--- dolg/SymbolAsyncIter.js
+++ dolg/SymbolAsyncIter.js
@@ -53,3 +53,16 @@ async function main1() {
 main1()
     .then(r => console.log(r))
     .catch(err => console.log(err))
+
+//--------------------------------------------------------------------------------------//
+//When a for await...of loop iterates over an iterable, it first gets the iterable's [@@asyncIterator]() method and calls it,
+// which returns an async iterator. If the @asyncIterator method does not exist, it then looks for an [@@iterator]() method,
+// which returns a sync iterator. The sync iterator returned is then wrapped into an async iterator by wrapping every object
+// returned from the next(), return(), and throw() methods into a resolved or rejected promise, with the value property resolved 
+//if it's also a promise. The loop then repeatedly calls the final async iterator's next() method and awaits the returned promise,
+// producing the sequence of values to be assigned to variable.
+//for await...of works on both sync and async iterables, while for...of only works on sync iterables.
+//for await...of can only be used in contexts where await can be used, which includes inside an async function body and in a module.
+//Even when the iterable is sync, the loop still awaits the return value for every iteration, leading to slower execution due to repeated promise unwrapping.
+//If the iterable is a sync iterable that yields promises, for await...of would produce a sequence of resolved values,
+//while for...of would produce a sequence of promises.
