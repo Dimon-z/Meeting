23f11c57254ff78e355d8a60c1a27f44b9a405ce
Dmitry Razumny <nepalevnodigital@gmail.com>
2023-09-02 13:25:29 +0300

    fetch

--- Fetch/fetchLjs.js
+++ Fetch/fetchLjs.js
@@ -1,2 +1,27 @@
-let promise = fetch(url, [options])
-//Без options это простой GET-запрос, скачивающий содержимое по адресу url.
+/* Создайте асинхронную функцию getUsers(names), 
+которая получает на вход массив логинов пользователей GitHub, 
+запрашивает у GitHub информацию о них и возвращает массив объектов-пользователей.
+ */
+async function getUsers(names) {
+    let jobs = [];
+  
+    for(let name of names) {
+      let job = fetch(`https://api.github.com/users/${name}`).then(
+        successResponse => {
+          if (successResponse.status != 200) {
+            return null;
+          } else {
+            return successResponse.json();
+          }
+        },
+        failResponse => {
+          return null;
+        }
+      );
+      jobs.push(job);
+    }
+  
+    let results = await Promise.all(jobs);
+  
+    return results;
+  }
